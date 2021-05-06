da987aa756866f132fcbe5944f732cbd4d92cffd
mrvaita <michele.tassoni@gmail.com>
2021-05-06 20:11:57 +0200

    finished dashboard mvp

--- dashboard.py
+++ dashboard.py
@@ -9,10 +9,11 @@ from dash.dependencies import Output, Input
 
 
 db = sqlite3.connect(Config.db_filename)
-df = pd.read_sql_query("SELECT * FROM ratio_foreigner_players", db)
-df1 = df.loc[df["league"] == "serie-a"]
-df2 = df.loc[df["league"] == "1-bundesliga"]
-leagues = df["league"].unique()
+df_foreigners = pd.read_sql_query("SELECT * FROM ratio_foreigner_players", db)
+df_averages = pd.read_sql_query("SELECT * FROM avg_players", db)
+leagues = df_foreigners["league"].unique()
+roles = df_averages["role"].unique()
+avgs = [col for col in df_averages.columns if "avg_" in col]
 
 app = dash.Dash(__name__)
 
@@ -31,7 +32,38 @@ app.layout = html.Div([
         dcc.Graph(
             id="foreigners-chart", config={"displayModeBar": False},
         )], className="card",
-    )
+    ),
+    html.Div([
+        html.Div([
+            dcc.Dropdown(
+                id='leaguefilter2',
+                options=[
+                    {'label': league, 'value': league} for league in leagues
+                ], value='serie-a'
+            ),
+		], style={'width': '33%', 'display': 'inline-block'}),
+        html.Div([
+            dcc.Dropdown(
+                id="rolefilter",
+                options=[
+                    {"label": role, "value": role} for role in roles
+                ], value="Goalkeeper"
+            ),
+		], style={"width": "33%", "display": "inline-block"}),
+        html.Div([
+            dcc.Dropdown(
+                id="avgfilter",
+                options=[
+                    {"label": avg, "value": avg} for avg in avgs
+                ], value="avg_height"
+            ),
+		], style={"width": "33%", "display": "inline-block"}),
+    ]),
+    html.Div([
+        dcc.Graph(
+            id="averages-chart", config={"displayModeBar": False},
+        )], className="card",
+    ),
 ])
 
 
@@ -40,7 +72,7 @@ app.layout = html.Div([
     Input("leaguefilter", "value"),
 )
 def update_chart(league):
-    filtered_df = df.loc[df["league"] == league, :]
+    filtered_df = df_foreigners.loc[df_foreigners["league"] == league, :]
     foreigners_chart_figure = {
         "data": [{
             "x": filtered_df["season"],
@@ -50,6 +82,30 @@ def update_chart(league):
     }
 
     return foreigners_chart_figure
+
+
+@app.callback(
+    Output("averages-chart", "figure"),
+    Input("leaguefilter2", "value"),
+    Input("rolefilter", "value"),
+    Input("avgfilter", "value")
+)
+def update_avg_chart(league, role, avg):
+    df_mask = (
+        (df_averages["league"] == league) &
+        (df_averages["role"] == role)
+    )
+    filtered_df = df_averages.loc[df_mask, :]
+
+    avg_chart_figure = {
+        "data": [{
+            "x": filtered_df["season"],
+            "y": filtered_df[avg],
+            "type": "lines",
+        }], "layout": {"title": f"Average {role} player {avg[4:]} for {league}"}
+    }
+
+    return avg_chart_figure
 
 
 if __name__ == "__main__":
