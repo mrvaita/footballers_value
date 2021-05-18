dd82ad3255ca7386270fdc685b733c113ea9a7b8
mrvaita <michele.tassoni@gmail.com>
2021-05-18 11:18:38 +0200

    Added docstrings to python modules

--- populate_db.py
+++ populate_db.py
@@ -1,3 +1,7 @@
+"""Triggers a prefect pipeline to collect players data for all the football
+seasons from 1970 until today.
+"""
+
 from datetime import datetime
 from prefect import Flow, task
 from scrape_transfermarkt import scrape_transfermarkt, get_season_urls
@@ -6,11 +10,28 @@ from prefect.run_configs import LocalRun
 
 @task(task_run_name="get season urls")
 def get_competition_urls(season):
+    """Fetches competition urls for a given season.
+
+    Args:
+        season: An integer representing the current season.
+
+    Returns:
+        A list including string urls for all the competitions.
+    """
+    
     return get_season_urls(season)
 
 
 @task(task_run_name="scrape season: {season}")
 def populate_season(league_urls, season):
+    """Triggers the prefect pipeline to insert players data, for the current
+    season to the database.
+
+    Args:
+        league_urls: A list containing league urls for the given season.
+        season: An integer representing the current season.
+    """
+
     flow = scrape_transfermarkt(league_urls, season)
 
     flow.run()
